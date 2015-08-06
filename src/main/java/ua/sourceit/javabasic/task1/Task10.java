cefd0f4ca1041bdb515efd6de7e2d08271894eac
Garagylja <kein85@gmail.com>
2015-08-06 09:19:14 +0300

    Pascal triangle

--- src/main/java/ua/sourceit/javabasic/task1/Task10.java
+++ src/main/java/ua/sourceit/javabasic/task1/Task10.java
@@ -11,6 +11,36 @@ package ua.sourceit.javabasic.task1;
  * Created by user on 04.08.2015.
  */
 public class Task10 {
+    public static void main(String[] args) {
+
+        printTriangleOfPascal(10);
+    }
+
+    public static void printTriangleOfPascal(int numStr) {
+        int[][] array = new int[numStr][];
+        int a = 1;
+        for (int n = 0; n < numStr; n++) {
+            array[n] = new int[a++];
+            for (int m = 0; m < array[n].length; m++) {
+                // Вычисляеться биноминальный коэффициент по формуле - n!/m!(n-m)!
+                array[n][m] = getFact(n) / (getFact(m) * getFact((n - m)));
+            }
+        }
+        for (int n = 0; n < numStr; n++) {
+            for (int m = 0; m < array[n].length; m++) {
+                System.out.print(array[n][m] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Метод вычисления фаткориала используеться для подсчета биноминального коэффициента
+    public static int getFact(int fac) {
+        int res = 1;
+        for (int i = 1; i <= fac; i++)
+            res *= i;
+        return res;
+    }
 
 
 }
