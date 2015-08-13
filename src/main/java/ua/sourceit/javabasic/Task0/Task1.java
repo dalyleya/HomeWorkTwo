4bcc84c7b81390fd9228f0c2ef166dac342ed3ac
Garagylja <kein85@gmail.com>
2015-08-13 15:52:06 +0300

    Готовый класс Task 5 согласно ТЗ

--- src/main/java/ua/sourceit/javabasic/Task0/Task1.java
+++ src/main/java/ua/sourceit/javabasic/Task0/Task1.java
@@ -1,7 +1,5 @@
 package ua.sourceit.javabasic.Task0;
 
-import java.util.ArrayList;
-
 /**
  * Дан массив натуральных чисел. Найти сумму элементов, кратных данному K.
  * Created by user on 12.08.2015.
@@ -10,7 +8,7 @@ public class Task1 {
     public static void main(String[] args) {
         int[] arr = new int[100];
         int k = 3;
-        ArrayList evenConsistency = new ArrayList();
+        int res = 0;
         for (int n = 0; n < arr.length; n++) {
             arr[n] = (int) (Math.random() * 100);
             System.out.print(" " + arr[n]);
@@ -21,17 +19,10 @@ public class Task1 {
         System.out.println("");
         System.out.println("" + "----------------------------");
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 0) {
-                evenConsistency.add(arr[i]);
+            if (arr[i] % k == 0) {
+                res += arr[i];
             }
         }
-        if (evenConsistency.isEmpty()) {
-            System.out.println("no even sequences");
-        } else {
-            for (int i = 0; i < evenConsistency.size(); i++) {
-                System.out.println(" " + evenConsistency.get(i));
-            }
-        }
+        System.out.println(res);
     }
 }
-
--- src/main/java/ua/sourceit/javabasic/Task0/Task5.java
+++ src/main/java/ua/sourceit/javabasic/Task0/Task5.java
@@ -1,13 +1,45 @@
 package ua.sourceit.javabasic.Task0;
 
+import java.util.ArrayList;
+
 /**
  * Дана последовательность натуральных чисел a1, a2, ..., an.
  * Создать массив из четных чисел этой последовательности. Если таких чисел нет, то вывести сообщение об этом факте.
  * Created by user on 13.08.2015.
  */
 public class Task5 {
+
+
     public static void main(String[] args) {
-
+        int[] arr = new int[100];
+        int k = 3;
+        ArrayList evenConsistency = new ArrayList();
+        for (int n = 0; n < arr.length; n++) {
+            arr[n] = (int) (Math.random() * 100);
+            System.out.print(" " + arr[n]);
+            if (n % 20 == 0) {
+                System.out.println("");
+            }
+        }
+        System.out.println("");
+        System.out.println("" + "----------------------------");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                evenConsistency.add(arr[i]);
+            }
+        }
+        if (evenConsistency.isEmpty()) {
+            System.out.println("no even sequences");
+        } else {
+            for (int i = 0; i < evenConsistency.size(); i++) {
+                System.out.print(" " + evenConsistency.get(i));
+            }
+        }
     }
-
 }
+
+
+
+
+
+
