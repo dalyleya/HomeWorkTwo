6cb6dd41597b80079698146450b819dd44973ae6
Garagylja <kein85@gmail.com>
2015-08-17 13:52:40 +0300

    Готовые задания по строкам согласно ТЗ #7

--- src/main/java/ua/sourceit/javabasic/TaskStrings/TaskStr7.java
+++ src/main/java/ua/sourceit/javabasic/TaskStrings/TaskStr7.java
@@ -5,4 +5,15 @@ package ua.sourceit.javabasic.TaskStrings;
  * Created by user on 16.08.2015.
  */
 public class TaskStr7 {
+    public static void main(String[] args) {
+        String s = new String(" Про гра мист sss ass dddd fff . aaasssd sss");
+        s = s.substring(0, s.indexOf('.'));
+        String[] parts = s.split(" ");
+        for (int i = 0; i < parts.length; i++) {
+            if (parts[i].length() == 3) {
+                System.out.print(" " + parts[i]);
+            }
+        }
+    }
+
 }
