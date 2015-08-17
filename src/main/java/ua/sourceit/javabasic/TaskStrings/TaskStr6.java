3c865037e206850f3af6ea2c30a6ab820a5afa85
Garagylja <kein85@gmail.com>
2015-08-17 11:53:31 +0300

    Готовые задания по строкам согласно ТЗ #6

--- src/main/java/ua/sourceit/javabasic/TaskStrings/TaskStr6.java
+++ src/main/java/ua/sourceit/javabasic/TaskStrings/TaskStr6.java
@@ -2,7 +2,15 @@ package ua.sourceit.javabasic.TaskStrings;
 
 /**
  * Дана строка символов, среди которых есть двоеточие (:). Определить, сколько символов ему предшествует.
+ *
  * Created by user on 16.08.2015.
  */
 public class TaskStr6 {
+    public static void main(String[] args) {
+        String s = new String("Програмист asddddfff: ");
+        int charCount = 0;
+        String[] parts = s.split(":");
+        charCount = parts[0].length();
+        System.out.println("charCount = " + charCount);
+    }
 }
