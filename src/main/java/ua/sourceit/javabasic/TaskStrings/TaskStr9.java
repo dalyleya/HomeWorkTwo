5d41dc603d1de1344bcccbc945610de1963a262b
Garagylja <kein85@gmail.com>
2015-08-19 10:17:54 +0300

    Готовые задания по строкам согласно ТЗ #9

--- src/main/java/ua/sourceit/javabasic/TaskStrings/TaskStr9.java
+++ src/main/java/ua/sourceit/javabasic/TaskStrings/TaskStr9.java
@@ -5,4 +5,24 @@ package ua.sourceit.javabasic.TaskStrings;
  * Created by user on 16.08.2015.
  */
 public class TaskStr9 {
+
+	public static void main(String[] args) {
+
+		String str = "abc fabcgabch bca abcabc fgh abc  abc";
+
+		System.out.println(searchWord(str, "abc"));
+	}
+
+	public static int searchWord(String message, String word) {
+
+		message = message.toLowerCase();
+		word = word.toLowerCase();
+		int i = message.indexOf(word);
+		int count = 0;
+		while (i >= 0) {
+			count++;
+			i = message.indexOf(word, i + 1);
+		}
+		return count;
+	}
 }
