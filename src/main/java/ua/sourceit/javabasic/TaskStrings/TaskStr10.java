d8b8b76324cf7815f46617af366db5e18128719c
Garagylja <kein85@gmail.com>
2015-08-19 20:40:27 +0300

    Готовые задания по строкам согласно ТЗ #10

--- src/main/java/ua/sourceit/javabasic/TaskStrings/TaskStr10.java
+++ src/main/java/ua/sourceit/javabasic/TaskStrings/TaskStr10.java
@@ -5,4 +5,20 @@ package ua.sourceit.javabasic.TaskStrings;
  * Created by user on 16.08.2015.
  */
 public class TaskStr10 {
+
+	public static void main(String[] args) {
+
+		String str = new String("asdfff ssddd kkk skkkrrrrkk");
+		String[] tmp = str.split(" ");
+		int len = tmp.length;
+		int kCount = 0;
+		for (char ch : tmp[len - 1].toCharArray()) {
+			if (ch == 'k') {
+				kCount++;
+			}
+		}
+		System.out.println(kCount);
+
+	}
+
 }
